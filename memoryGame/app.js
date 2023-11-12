50b2c876be593abec0ad85404cd64a6584b7696d
Ekow <ekowsackey22@gmail.com>
2023-11-12 23:37:52 +0000

    added flip function and images

--- memoryGame/app.js
+++ memoryGame/app.js
@@ -1,3 +1,4 @@
+
 const cardArray = [
   {
     name: 'fries',
@@ -5,7 +6,7 @@ const cardArray = [
   },
   {
     name: 'burger',
-    img: 'images/burger.png'
+    img: 'images/cheeseburger.png'
   },
   {
     name: 'hotdog',
@@ -29,7 +30,7 @@ const cardArray = [
   },
   {
     name: 'burger',
-    img: 'images/burger.png'
+    img: 'images/cheeseburger.png'
   },
   {
     name: 'hotdog',
@@ -49,3 +50,34 @@ const cardArray = [
   }
 
 ];
+
+
+cardArray.sort(() => 0.5 - Math.random()); // randomize the cards
+
+
+
+const grid = document.querySelector('#grid');
+const cardsChosen = []
+
+function createBoard() {
+  for (let i = 0; i < cardArray.length; i++){
+    const card = document.createElement('img')
+    card.setAttribute('src', 'images/blank.png')
+    card.setAttribute('data-id', i)
+    grid.appendChild(card)
+    card.addEventListener('click', flipCard)
+
+
+  }
+}
+createBoard()
+
+function flipCard(){
+  const cardId= this.getAttribute('data-id')
+  cardsChosen.push(cardArray[cardId].name)
+  this.setAttribute('src', cardArray[cardId].img)
+
+
+
+
+}
