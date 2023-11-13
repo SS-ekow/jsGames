6a23604770a429f1c4f4d3f49fd27f10cca92139
Ekow <ekowsackey22@gmail.com>
2023-11-13 00:20:43 +0000

    finished logic

--- memoryGame/app.js
+++ memoryGame/app.js
@@ -57,7 +57,10 @@ cardArray.sort(() => 0.5 - Math.random()); // randomize the cards
 
 
 const grid = document.querySelector('#grid');
-const cardsChosen = []
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = [];
+let cardsChosenIds = [];
+const cardsWon = [];
 
 function createBoard() {
   for (let i = 0; i < cardArray.length; i++){
@@ -72,11 +75,53 @@ function createBoard() {
 }
 createBoard()
 
+function checkMatch(){
+  const cards = document.querySelectorAll('img')
+  const optionOneId = cardsChosenIds[0]
+  const optionTwoid = cardsChosen[1]
+
+  if (optionOneId == optionTwoid){
+    cards[optionOneId].setAttribute('src', 'images/blank.png')
+    cards[optionTwoid].setAttribute('src', 'images/blank.png')
+
+    alert('You have clicked the same card!')
+  }
+
+
+
+ if ( cardsChosen[0] == cardsChosen[1]){
+  alert('you found a match!')
+  cards[optionOneId.setAttribute('src', 'images/white.png')]
+  cards[optionTwoid.setAttribute('src', 'images/white.png')]
+  cards[optionOneId.removeEventListener('click', flipCard)]
+  cards[optionTwoid.removeEventListener('click', flipCard)]
+  cardsWon.push(cardsChosen)
+ }
+ else {
+  cards[optionOneId.setAttribute('src', 'images/blank.png')]
+  cards[optionTwoid.setAttribute('src', 'images/blank.png')]
+  alert('sorry try again')
+
+
+ }
+ resultDisplay.textContent = cardsWon.length
+ cardsChosen = []
+ cardsChosenIds = []
+
+ if (cardsWon.length == cardArray.length/2){
+  resultDisplay.innerHTML = 'Congratulations. you found them all!'
+ }
+}
+
 function flipCard(){
   const cardId= this.getAttribute('data-id')
   cardsChosen.push(cardArray[cardId].name)
+  cardsChosenIds.push(cardId)
   this.setAttribute('src', cardArray[cardId].img)
 
+  if(cardsChosen.length === 2){
+    setTimeout(checkMatch, 500)
+  }
 
 
 
